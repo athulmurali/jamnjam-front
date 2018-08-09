8f9d4ca0bb3822ec3a55058a521267f3b1cb6032
Athul Muralidharan <athulmurali@gmail.com>
2018-08-09 20:30:09 +0530

    Added boostrap to dependecies

---
+++ src/containers/userRegistration/VerifyEmail.js
@@ -0,0 +1,17 @@
+const VerifyEmail = ()=>{
+    return(
+        <div className="card rounded" id="successCard" style="display: none;">
+            <div className="card-img-top row">
+
+                <i className="mx-auto fas fa-check-circle fa-6x" style="padding: 10%"></i>
+            </div>
+
+            <div className="card-body ">
+                <h5 className="card-title">Registration Successful! </h5>
+                <p className="card-text">An activation mail has been sent to your registered email. It should be there within 5 minutes. Please check your inbox. </p>
+                <a href="#" className="btn btn-primary">Login</a>
+            </div>
+        </div>
+
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+import $ from 'jquery';
+import Popper from 'popper.js';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
 import RoutedApp from "./RoutedApp";
