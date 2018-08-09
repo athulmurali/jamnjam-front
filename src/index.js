9dd46a364077e92b0517c69bb68db1d65455b758
Athul Muralidharan <athulmurali@gmail.com>
2018-08-10 02:10:09 +0530

    User registration form validation

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
 
 
 import './index.css';
